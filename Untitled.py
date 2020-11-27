707736d0acd3cc0b1b1b63bf34d3df828162556a
CleverRatan <67803567+ratan10@users.noreply.github.com>
2020-11-27 21:32:58 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+from matplotlib import pyplot as plt
+plt.plot([1,2,3], [2,5,7])
+plt.show()
+
+
+# In[ ]:
+
+
+
+
